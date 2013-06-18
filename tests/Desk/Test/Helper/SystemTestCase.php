b7b858dbabceda897a6b9a627ff4484004f8475a
Brad Feehan <git@bradfeehan.com>
2013-06-18 18:39:22 +1000

    Add logClient() to system tests to log requests

--- tests/Desk/Test/Helper/SystemTestCase.php
+++ tests/Desk/Test/Helper/SystemTestCase.php
@@ -3,7 +3,22 @@
 namespace Desk\Test\Helper;
 
 use Desk\Test\Helper\TestCase;
+use Guzzle\Plugin\Log\LogPlugin;
+use Guzzle\Service\Client;
 
 abstract class SystemTestCase extends TestCase
 {
+
+    /**
+     * Adds a LogPlugin to a Guzzle service client object
+     *
+     * This will cause any requests created by the client to log all
+     * requests sent and received.
+     *
+     * @param Guzzle\Service\Client $client The Guzzle service client
+     */
+    protected function logClient(Client $client)
+    {
+        $client->addSubscriber(LogPlugin::getDebugPlugin());
+    }
 }

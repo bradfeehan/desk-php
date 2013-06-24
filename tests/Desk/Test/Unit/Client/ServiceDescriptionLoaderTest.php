dc4c943641e6e5fca032d15bd42b84f08228ba0a
Brad Feehan <git@bradfeehan.com>
2013-06-25 03:11:50 +1000

    Improve inheritance of model properties
    Add to the ServiceDescriptionLoader to ensure that models which inherit
    from another model (where both models have properties) correctly
    inherit both their own properties and those from their parent model,
    rather than just their own.

--- tests/Desk/Test/Unit/Client/ServiceDescriptionLoaderTest.php
+++ tests/Desk/Test/Unit/Client/ServiceDescriptionLoaderTest.php
@@ -18,6 +18,128 @@ class ServiceDescriptionLoaderTest extends UnitTestCase
     }
 
     /**
+     * @covers Desk\Client\ServiceDescriptionLoader::build
+     */
+    public function testBuild()
+    {
+        $fooProp = \Mockery::mock('Guzzle\\Service\\Description\\Parameter');
+        $barProp = \Mockery::mock('Guzzle\\Service\\Description\\Parameter')
+            ->shouldReceive('getName')
+                ->andReturn('barProperty')
+            ->getMock();
+
+        $model = \Mockery::mock('Guzzle\\Service\\Description\\Parameter')
+            ->shouldReceive('getProperties')
+                ->andReturn(array('fooProperty' => $fooProp))
+            ->shouldReceive('getProperty')
+                ->with('barProperty')
+                ->andReturn(null)
+            ->shouldReceive('addProperty')
+                ->with($barProp)
+                ->once()
+            ->getMock();
+
+        $model->extends = 'myParentModel';
+
+        $parent = \Mockery::mock('Guzzle\\Service\\Description\\Parameter')
+            ->shouldReceive('getProperties')
+                ->andReturn(array('barProperty' => $barProp))
+            ->getMock();
+
+        $descriptionClass = 'Guzzle\\Service\\Description\\ServiceDescription';
+        $description = \Mockery::mock($descriptionClass)
+            ->shouldReceive('getModels')
+                ->andReturn(array($model))
+            ->shouldReceive('getModel')
+                ->with('myParentModel')
+                ->andReturn($parent)
+            ->getMock();
+
+        $loader = $this->mock()
+            ->shouldReceive('parentBuild')
+                ->with(array(), array())
+                ->andReturn($description)
+            ->getMock();
+
+        $result = $this->call($loader, 'build', array(array(), array()));
+
+        $this->assertSame($description, $result);
+    }
+
+    /**
+     * @covers Desk\Client\ServiceDescriptionLoader::build
+     */
+    public function testBuildWithModelWithoutProperties()
+    {
+        $model = \Mockery::mock('Guzzle\\Service\\Description\\Parameter');
+
+        $descriptionClass = 'Guzzle\\Service\\Description\\ServiceDescription';
+        $description = \Mockery::mock($descriptionClass)
+            ->shouldReceive('getModels')
+                ->andReturn(array($model))
+            ->getMock();
+
+        $loader = $this->mock()
+            ->shouldReceive('parentBuild')
+                ->with(array(), array())
+                ->andReturn($description)
+            ->getMock();
+
+        $result = $this->call($loader, 'build', array(array(), array()));
+
+        $this->assertSame($description, $result);
+    }
+
+    /**
+     * @covers Desk\Client\ServiceDescriptionLoader::build
+     */
+    public function testBuildWithModelWithoutParentProperties()
+    {
+        $fooProp = \Mockery::mock('Guzzle\\Service\\Description\\Parameter');
+
+        $model = \Mockery::mock('Guzzle\\Service\\Description\\Parameter')
+            ->shouldReceive('getProperties')
+                ->andReturn(array('fooProperty' => $fooProp))
+            ->getMock();
+
+        $model->extends = 'myParentModel';
+
+        $parent = \Mockery::mock('Guzzle\\Service\\Description\\Parameter')
+            ->shouldReceive('getProperties')
+                ->andReturn(array())
+            ->getMock();
+
+        $descriptionClass = 'Guzzle\\Service\\Description\\ServiceDescription';
+        $description = \Mockery::mock($descriptionClass)
+            ->shouldReceive('getModels')
+                ->andReturn(array($model))
+            ->shouldReceive('getModel')
+                ->with('myParentModel')
+                ->andReturn($parent)
+            ->getMock();
+
+        $loader = $this->mock()
+            ->shouldReceive('parentBuild')
+                ->with(array(), array())
+                ->andReturn($description)
+            ->getMock();
+
+        $result = $this->call($loader, 'build', array(array(), array()));
+
+        $this->assertSame($description, $result);
+    }
+
+    /**
+     * @covers Desk\Client\ServiceDescriptionLoader::parentBuild
+     */
+    public function testParentBuild()
+    {
+        $result = $this->mock('parentBuild')->parentBuild(array(), array());
+        $descriptionClass = 'Guzzle\\Service\\Description\\ServiceDescription';
+        $this->assertInstanceOf($descriptionClass, $result);
+    }
+
+    /**
      * @covers Desk\Client\ServiceDescriptionLoader::resolveExtension
      */
     public function testResolveExtension()
@@ -56,12 +178,8 @@ class ServiceDescriptionLoaderTest extends UnitTestCase
             ),
         );
 
-        // All this, just to call a protected method passing args by reference!
         $loader = $this->mock();
-        $reflectionLoader = new ReflectionObject($loader);
-        $method = $reflectionLoader->getMethod('resolveExtension');
-        $method->setAccessible(true);
-        $method->invokeArgs($loader, array('fooOperation', &$op, &$operations));
+        $this->call($loader, 'resolveExtension', array('fooOperation', &$op, &$operations));
 
         // Should have its original fooLink
         $this->assertTrue(isset($op['data']['links']['fooLink']));
@@ -106,15 +224,29 @@ class ServiceDescriptionLoaderTest extends UnitTestCase
         );
 
         $loader = $this->mock();
-        $reflectionLoader = new ReflectionObject($loader);
-        $method = $reflectionLoader->getMethod('resolveExtension');
-        $method->setAccessible(true);
-        $method->invokeArgs($loader, array('fooOperation', &$op, &$operations));
+        $this->call($loader, 'resolveExtension', array('fooOperation', &$op, &$operations));
 
         $this->assertTrue(isset($op['parameters']['fooParameter']['description']));
         $this->assertSame('fooDescription', $op['parameters']['fooParameter']['description']);
 
         $this->assertTrue(isset($op['parameters']['barParameter']['description']));
         $this->assertSame('barDescription', $op['parameters']['barParameter']['description']);
+    }
+
+    /**
+     * Calls a private method on any object
+     *
+     * @param mixed  $object     The object to call the method on
+     * @param string $methodName The name of the method to call
+     * @param array  $args       Arguments to pass to the method
+     *
+     * @return mixed
+     */
+    private function call($object, $methodName, $args = array())
+    {
+        $reflectionLoader = new ReflectionObject($object);
+        $method = $reflectionLoader->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs($object, $args);
     }
 }

106778533727499d14122be4e31e6713f23d4006
David Guillot <david.guillot@mylittleparis.com>
2014-01-09 11:25:50 +0100

    Fixed EmbeddedCommand to extend AbstractCommand, fixed tests accordingly

--- lib/Desk/Relationship/Resource/EmbeddedCommand.php
+++ lib/Desk/Relationship/Resource/EmbeddedCommand.php
@@ -2,71 +2,37 @@
 
 namespace Desk\Relationship\Resource;
 
-use Guzzle\Common\Collection;
-use Guzzle\Http\Message\RequestInterface;
-use Guzzle\Http\Message\Response;
-use Guzzle\Service\ClientInterface;
 use Guzzle\Service\Command\CommandInterface;
-use Guzzle\Service\Description\Operation;
+use Guzzle\Service\Command\AbstractCommand;
 
-class EmbeddedCommand implements CommandInterface
+class EmbeddedCommand extends AbstractCommand implements CommandInterface
 {
-
     /**
-     * The client associated with this command
      *
-     * @var Guzzle\Service\ClientInterface
-     */
-    protected $client;
-
-    /**
-     * The operation associated with this command
-     *
-     * @var Guzzle\Service\Description\Operation
-     */
-    protected $operation;
-
-    /**
-     * The request object
-     *
-     * @var mixed
-     */
-    protected $request;
-
-    /**
-     * The response object
-     *
-     * @var mixed
+     * @var EmbeddedResponse
      */
     protected $response;
 
     /**
-     * The result object
      *
-     * @var mixed
+     * @param EmbeddedResponse $response
+     *
+     * @return EmbeddedCommand
      */
-    protected $result;
-
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getName()
+    public function setResponse(EmbeddedResponse $response)
     {
-        return $this->getOperation()->getName();
+        $this->response = $response;
+
+        return $this;
     }
 
     /**
-     * {@inheritdoc}
+     *
+     * @return EmbeddedResponse
      */
-    public function getOperation()
+    public function getResponse()
     {
-        if (!$this->operation) {
-            $name = 'EmbeddedCommand';
-            $this->operation = new Operation(array('name' => $name));
-        }
-
-        return $this->operation;
+        return $this->response;
     }
 
     /**
@@ -88,70 +54,6 @@ class EmbeddedCommand implements CommandInterface
     /**
      * {@inheritdoc}
      */
-    public function getClient()
-    {
-        return $this->client;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setClient(ClientInterface $client)
-    {
-        $this->client = $client;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRequest()
-    {
-        return $this->request;
-    }
-
-    /**
-     * Sets the request to use for this command
-     */
-    public function setRequest(RequestInterface $request)
-    {
-        $this->request = $request;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getResponse()
-    {
-        return $this->response;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setResponse(Response $response)
-    {
-        $this->response = $response;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getResult()
-    {
-        return $this->result;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setResult($result)
-    {
-        $this->result = $result;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     public function isPrepared()
     {
         return true;
@@ -160,24 +62,8 @@ class EmbeddedCommand implements CommandInterface
     /**
      * {@inheritdoc}
      */
-    public function prepare()
+    protected function build()
     {
-        return $this->getRequest();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getRequestHeaders()
-    {
-        return new Collection();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setOnComplete($callable)
-    {
-        // take no action
+        // do nothing, it will never be called
     }
 }
